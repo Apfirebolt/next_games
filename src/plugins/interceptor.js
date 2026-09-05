83d00e96c8bd70e731fbf6a53f49fa7577ef6115
Apfirebolt <aspper20@gmail.com>
2026-09-05 14:59:14 +0530

    cloudinary bug fix

--- src/plugins/interceptor.js
+++ src/plugins/interceptor.js
@@ -2,10 +2,10 @@ import axios from 'axios';
 import Cookies from "js-cookie";
 
 // for dev
-// let baseURL = 'http://localhost:3000/api/';
+let baseURL = 'http://localhost:3000/api/';
 
 // for prod
-let baseURL = 'https://www.codelean.in/api/';
+// let baseURL = 'https://www.codelean.in/api/';
 
 const httpClient = axios.create({ baseURL });
 
